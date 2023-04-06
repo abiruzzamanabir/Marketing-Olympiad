ff27fc5957962d29b3ae1bce39e384f995545f04
Shakil Hussain <shakil550sh@gmail.com>
2023-04-07 00:38:50 +0600

    pdf generate

---
+++ config/pdf.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'font_path' => public_path('assets/fonts/'),
+    'font_data' => [
+        'GreatVibes' => [
+            'R' => 'GreatVibes_regular.ttf',
+        ],
+        'Montserrat' => [
+            'R' => 'Montserrat_regular.ttf',
+            'B' => 'Montserrat_bold.ttf',
+            'E' => 'Montserrat_ExtraLight.ttf',
+        ],
+    ],
+];
---
+++ resources/views/admin/mail/certificate.blade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Account Information Update</title>
+    <style>
+        /*@import url("https://fonts.googleapis.com/css2?family=Great+Vibes&family=Montserrat:wght@200;400;500;700&display=swap");*/
+
+        @font-face {
+            font-family: 'GreatVibes';
+{{--            src: url({{ public_path('assets\fonts\GreatVibes-Regular.ttf') }}) format("truetype");--}}
+            font-weight: 400; // use the matching font-weight here ( 100, 200, 300, 400, etc).
+        font-style: normal; // use the matching font-style here
+        }
+        body {
+            font-family: "GreatVibes";
+            font-size: 16px;
+            line-height: 1.4;
+            color: #333333;
+
+            padding: 0;
+            margin: 0;
+        }
+
+        .container {
+            text-align: center;
+
+            max-width: 1056px;
+            margin: 0 auto;
+            padding: 10px;
+            background-color: #ffffff;
+            box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
+            border-radius: 5px;
+            {{--background-image: linear-gradient(--}}
+            {{--        rgb(255, 255, 255, 0.8),--}}
+            {{--        rgb(255, 255, 255, 0.8)--}}
+            {{--);--}}
+            {{--url("{{ __DIR__ . '/../../../../public/storage/logo/Marketing-Olympiad-Logo-FINAL.png'}}");--}}
+            {{--background-repeat: no-repeat;--}}
+            {{--background-size: calc(130%);--}}
+            {{--background-position: left center;--}}
+            {{--background-color: rgba(255, 255, 255, 0.95);--}}
+        }
+
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 15%;
+        }
+
+        h1 {
+            font-size: 60px;
+            margin-top: 0;
+            margin-bottom: 20px;
+            text-align: center;
+            color: #000000;
+        }
+
+        p {
+            margin: 0 0 20px;
+            text-align: justify;
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            font-weight: bold;
+            text-decoration: none;
+            color: #ffffff;
+            background-color: #0085ff;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+
+        .button:hover {
+            background-color: #0071d1;
+        }
+
+        .details {
+            margin-bottom: 20px;
+            padding: 20px;
+            background-color: #f5f5f58f;
+            border-radius: 5px;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .details ul {
+            margin: 0;
+            padding: 0;
+            list-style-type: none;
+        }
+
+        .details li {
+            margin: 0 0 10px;
+            padding: 0;
+            font-weight: bold;
+        }
+
+        .details li span {
+            font-weight: normal;
+        }
+    </style>
+</head>
+
+<body style="font-family:Montserrat,sans-serif">
+<div class="container">
+    <img
+            src="https://bbf.digital/marketing-olympiad/public/storage/logo/logo.png"
+            alt=""
+    />
+    <h1 style="font-family: 'GreatVibes', cursive; font-weight: 200">
+        Certificate Of Participation
+    </h1>
+    <div style="line-height: 20px">
+        <h3
+                style="
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 200;
+            font-size: 30px;
+          "
+                class="center"
+        >
+            This certificate is awarded to
+        </h3>
+        <h3 style="font-family: 'Montserrat', sans-serif;font-weight: 500;font-size: 30px;text-transform: uppercase;"class="center">
+            Name Of The Person
+        </h3>
+        <h3
+                style="
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 200;
+            font-size: 30px;
+          "
+                class="center"
+        >
+            for participating in the
+        </h3>
+        <h3
+                style="
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 400;
+            font-size: 30px;
+          "
+                class="center"
+        >
+            MARKETING OLYMPIAD
+        </h3>
+    </div>
+    <img
+            style="
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          width: 100px;
+        "
+            src="https://bbf.digital/marketing-olympiad/public/storage/logo/signature.png"
+            alt=""
+    />
+    <div style="line-height: 0px">
+        <h1 style="font-size: 14px">SHARIFUL ISLAM</h1>
+        <h4 style="
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 400;
+            font-size: 15px;
+          " class="center">FOUNDER & MANAGING DIRECTOR</h4>
+        <h4 style="
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 400;
+            font-size: 15px;
+          " class="center">BANGLADESH BRAND FORUM</h4>
+    </div>
+    <img
+            style="
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          width: 300px;
+        "
+            src="https://bbf.digital/marketing-olympiad/public/storage/logo/logo_panel.png"
+            alt=""
+    />
+</div>
+</body>
+</html>
