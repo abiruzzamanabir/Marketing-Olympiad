7ac62e1b6f547d37a5f294665fec6da4ad5f0ac9
Shakil Hussain <shakil550sh@gmail.com>
2023-04-12 00:45:14 +0600

    notification and question option seected backgound color

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('/student-block', [StudentController::class, 'blockStudent'])->name('student.block');
     Route::get('/student-destroy/{id}', [StudentController::class, 'destroyStudent'])->name('student.destroy');
     Route::get('/student-ban/{id}', [StudentController::class, 'banStudent'])->name('student.ban');
-    Route::post('/send-mail', [ExamController::class, 'examTimeSendMailAll'])->name('exam.time.mail');
+    Route::get('/send-mail', [ExamController::class, 'examTimeSendMailAll'])->name('exam.time.mail');
 
 
 });
